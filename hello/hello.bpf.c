c53e2a06456a989c561605a5b062a6f0ac7d63f4
theanarkh <2923878201@qq.com>
2021-11-28 02:26:48 +0800

    init

---
+++ hello/hello.bpf.c
@@ -0,0 +1,14 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int handle_tp(void *ctx)
+{
+    int pid = bpf_get_current_pid_tgid()>> 32;
+    char fmt[] = "BPF triggered from PID %d.\n";
+    bpf_trace_printk(fmt, sizeof(fmt), pid);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
